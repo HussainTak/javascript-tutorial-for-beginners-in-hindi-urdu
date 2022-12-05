281a7b808dbe7083643d8018fa3bd57d4e74abb9
hussain tak <hussaintak478@gmail.com>
2022-12-05 22:07:01 +0530

    work change learned new method

---
+++ js/datatype.js
@@ -0,0 +1,4 @@
+var x = "-34.50"
+document.write(x + "<br>");
+document.write(typeof x);
+  
---
+++ js/var.js
@@ -0,0 +1,13 @@
+document.write("hussain <br>")
+var z="yahoo baba";
+document.write(z);
+let  firstname="junad <br>";
+firstname=" <br>husan";
+document.write(firstname + "</br>");
+//constant variable
+
+const hussain = "shanwaz";
+document.write(hussain)
+
+// hussain = "faazz"
+// document.write(hussain);
